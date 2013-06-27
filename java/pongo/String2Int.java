9c2792652b1023e24476817397a176f0621903f1
tsui <tsui.uestc@gmail.com>
2013-06-27 14:39:06 +0800

    add author

--- java/pongo/String2Int.java
+++ java/pongo/String2Int.java
@@ -2,6 +2,9 @@
  * 输入一个表示整数的字符串，把该字符串转换成整数并输出，
  * 例如输入字符串"345"，则输出整数345。
  * 请完成函数StrToInt，实现字符串转换成整数的功能。
+ *
+ * @author xmtsui
+ * @version v1.0
  */
 import java.util.regex.Matcher;
 import java.util.regex.Pattern;
